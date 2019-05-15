c3f36778336538efd62c86b855df596d97ea4817
Daniel Benavides <daniel.benavides@wolox.co>
2019-05-15 10:39:55 -0500

    Added testing and documentation for the users endpoint

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,12 +5,11 @@ const logger = require('../logger'),
 exports.userRegistration = (req, res, next) => {
   logger.info('POST method start. User registration.');
   const user = req.body;
-  logger.info(user);
   return usersMapper
     .encryptAndFormat(user)
     .then(users.registerUser)
     .then(response => {
-      logger.info(`User inserted with ID: ${response.id}`, user);
+      logger.info(`User inserted with ID: ${response.id}`);
       return res.status(201).send({ message: `User ${user.name}} inserted with ID: ${response.id}` });
     })
     .catch(next);
