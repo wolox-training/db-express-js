c3b30b4e2cd43091fb93fd81f13ee6db873a0687
Daniel Benavides <daniel.benavides@wolox.co>
2019-05-15 10:38:31 -0500

    Revert "Requested changes, apyling schema validations"
    This reverts commit 532fb8669632b59a60878285d9054b30d4dafb73.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,11 +5,12 @@ const logger = require('../logger'),
 exports.userRegistration = (req, res, next) => {
   logger.info('POST method start. User registration.');
   const user = req.body;
+  logger.info(user);
   return usersMapper
     .encryptAndFormat(user)
     .then(users.registerUser)
     .then(response => {
-      logger.info(`User inserted with ID: ${response.id}`);
+      logger.info(`User inserted with ID: ${response.id}`, user);
       return res.status(201).send({ message: `User ${user.name}} inserted with ID: ${response.id}` });
     })
     .catch(next);
