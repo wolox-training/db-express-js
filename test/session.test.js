c1fe51bfc4cd17c9b7f7e3e61ed6b513295efaf4
Daniel Benavides <daniel.benavides@wolox.co>
2019-05-16 11:40:47 -0500

    Added user authentication and tests

--- test/session.test.js
+++ test/session.test.js
@@ -2,9 +2,9 @@ const request = require('supertest'),
   app = require('../app');
 
 describe('POST /users/sessions. Test user log in', () => {
-  test('It should show the uptime', () => {
+  test('It should respond with code 200', () => {
     const agent = request(app);
-    agent
+    return agent
       .post('/users')
       .send({
         name: 'Lucia',
@@ -12,16 +12,66 @@ describe('POST /users/sessions. Test user log in', () => {
         email: 'luci.alvarez@wolox.ar',
         password: '1234_wol0x'
       })
-      .then(() => {
+      .then(() =>
         agent
           .post('/users/sessions')
           .send({
             email: 'luci.alvarez@wolox.ar',
-            password: '123_wol0x'
+            password: '1234_wol0x'
           })
           .then(response => {
             expect(response.statusCode).toBe(200);
-          });
-      });
+          })
+      );
+  });
+});
+
+describe('POST /users/sessions. Test log in fail due to wrong email', () => {
+  test('It should respond with 403', () => {
+    const agent = request(app);
+    return agent
+      .post('/users')
+      .send({
+        name: 'Lucia',
+        last_name: 'Alvarez',
+        email: 'luci.alvarez@wolox.ar',
+        password: '1234_wol0x'
+      })
+      .then(() =>
+        agent
+          .post('/users/sessions')
+          .send({
+            email: 'luci.alrez@wolox.ar',
+            password: '1234_wol0x'
+          })
+          .then(response => {
+            expect(response.statusCode).toBe(403);
+          })
+      );
+  });
+});
+
+describe('POST /users/sessions. Test log in fail due to wrong password', () => {
+  test('It should respond with 403', () => {
+    const agent = request(app);
+    return agent
+      .post('/users')
+      .send({
+        name: 'Lucia',
+        last_name: 'Alvarez',
+        email: 'luci.alvarez@wolox.ar',
+        password: '1234_wol0x'
+      })
+      .then(() =>
+        agent
+          .post('/users/sessions')
+          .send({
+            email: 'luci.alvarez@wolox.ar',
+            password: '1_wol0xxx0'
+          })
+          .then(response => {
+            expect(response.statusCode).toBe(403);
+          })
+      );
   });
 });
